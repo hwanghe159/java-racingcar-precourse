c2d44b5ad1794267c3ceceeb4c096fb504b5bb3b
junho <hwanghe159@naver.com>
2019-12-20 23:08:23 +0900

    main함수에서 실행할 RacingGame클래스 내부의 play()생성

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -3,8 +3,8 @@ package domain;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		RacingGame game = new RacingGame();
+		game.play();
 	}
 
 }
