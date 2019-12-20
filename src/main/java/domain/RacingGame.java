4f7c55b76901b24e8f4131eaa47e9cf4ac2ccaab
junho <hwanghe159@naver.com>
2019-12-20 23:09:35 +0900

    play()를 전제적인 흐름에 따라 prepare(), gamePlay(), result()로 분리

--- src/main/java/domain/RacingGame.java
+++ src/main/java/domain/RacingGame.java
@@ -2,6 +2,23 @@ package domain;
 
 public class RacingGame {
 	public void play() {
+		prepare();
+		gamePlay();
+		result();
+	}
+
+	private void result() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void gamePlay() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void prepare() {
+		// TODO Auto-generated method stub
 		
 	}
 }
